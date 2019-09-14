7e90b1a9e61cd21710d4908660bc21421375c21a
Angel Santillan <angelsb94@gmail.com>
2019-09-13 20:44:43 -0500

    primer ejercicio de Vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <template lang="pug">
   #app
-    img(src="./assets/logo.png")
-    h1 {{ msg }}
-    div(class='container')
-      img(src="./assets/logo_platzi.png")
-      h1(class="Titulo") {{ name_form }}
+    section(class="section")
+      div(class="container")
+        img(src="./assets/logo_platzi.png")
+        h1(class="title")
+          b Registro de estacion Platzi
+        form
+          .field
+            label(class="label") Nombre
+            .control
+              input(class="input is-complete" type="text" v-model="register.name")
+          .field
+            label(class="label") Apellido
+            .control
+              input(class="input is-complete" type="text" v-model="register.lastname")
+          .field
+            label(class="label") Cargo
+            .control
+              input(class="input is-complete" type="text" v-model="register.position")
+          div(class="field is-grouped is-grouped-right")
+            p(class="control")
+              a(class="button is-primary is-large") Enviar
 </template>
 
 <script>
@@ -12,14 +28,37 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Hello word angel',
-      name_form: "Registro estacion platzi" 
+      register: {
+        name: '',
+        lastname: '',
+        position: ''
+      }
     }
   }
 }
 </script>
-
 <style lang="scss">
-@import './scss/main.scss'
+  @import './scss/main.scss';
+  $color-first:#a3ecf3;
+  $color-secondary:#00BFD6;
+  $color-third:#212121;
+  $color-fourth:#dd9300;
+
+  .title{
+    color:$color-secondary;
+
+  }
+  .is-complete{
+    border-color: white;
+    border-bottom:2px solid $color-fourth;
+  }
+  .label{
+    color:$color-fourth;
+  }
+  .container{
+    display:flex;
+    flex-flow:column wrap;
+    align-content:center;
+  }
 
 </style>
