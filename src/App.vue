6257a41348c511dad1db3ae254f0c609b8cda9f7
Angel Santillan <angelsb94@gmail.com>
2019-09-16 21:13:07 -0500

    Eventos

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <template lang="pug">
   #app
-    p Name
     input(v-model="name")
-    br
-    p Last name
-    input(v-model="last_name")
-    br
-    p Full name: {{ fullName }}
+    button(v-on:click="format") Format
 
-    br
-    p Birthday
-    input(v-model="birthday" placeholder="YYYY-MM-DD")
-    p {{ todayAge }}
+
+    p {{ formatName }}
 </template>
 
 <script>
@@ -20,39 +13,22 @@ export default {
   data () {
     return {
       name: '',
-      last_name: '',
-      birthday: ''
+
+      formatName: '',
     }
   },
-
-
-
-  computed: {
-    fullName () {
-      return`${ this.name}${ this.last_name }`;
-    },
-
-    todayAge () {
-      let date_birth = this.birthday;
-      let actual_date = new Date();
-      let current_year = actual_date.getFullYear()
-      if(date_birth.match(/^[\d0-9]{4}[-]+[\d0-9]{2}[-]+[\d0-9]{2}$/)){
-        let year = date_birth.split('-')[0];
-        let age = current_year - year;
-        return (age > 0 ? `Actual age: ${ age }` : `Only dates before ${ current_year }`);
-      } else {
-        return'Birthday format doesn\'t match';
-      }
-    }
-  },
-  watch: {
-    name (newVal, oldVal) {
-      console.log(newVal, oldVal)
+  methods: {
+    format() {
+      this.formatName = this.name.split(' ').join('-').toUpperCase()
     }
   }
+
 }
 </script>
 
 <style lang="scss">
   @import'./scss/main.scss';
+
+
+
 </style>
